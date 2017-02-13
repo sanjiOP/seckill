d5ccaa42e819f60bbc1b40abc1af9e119a01e7da
lbaztyy@126.com <lbaztyy@126.com>
2017-02-13 10:56:14 +0800

    修改秒杀url

--- buy.php
+++ buy.php
@@ -1,5 +1,4 @@
 <?php
 	
-	//$url = 'http://192.168.16.73/redis/index.php?app=app&c=order&a=buypub&gid=1';
-	$url = 'http://192.168.16.73/redis/index.php?app=app&c=order&a=buyredis&gid=1';
+	$url = 'http://192.168.16.73/Seckill/index.php?app=app&c=seckill&a=addQsec&gid=1&type=redis';
 	file_get_contents($url);
